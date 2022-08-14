4fa88e26be8236cfcf4179f3c55b4b67dc85cfde
Артём Тимашков <bour@yandex.ru>
2022-08-14 12:57:38 +0300

    Add titles, categories, genres + urls and views

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,15 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+
+router = DefaultRouter()
+router.register('titles', TitleViewSet, basename='titles')
+router.register('categories', CategoryViewViewSet, 'categories')
+router.register('genres', GenreViewSet, basename='genres')
+
 
 urlpatterns = [
     path('v1/', include('users.urls')),
+    path('v1/', include(router.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import filters, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from review.models import Category, Genre, Title
+
+from .permissions import IsAdminOrReadOnly
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'genre', 'name', 'year')
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',) 
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',) 
+
+
+
+
