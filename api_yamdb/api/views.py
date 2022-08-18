2dc794926aa33da85210fff5e5047bcae2249d9a
Алексей Вернер <werneralexey@mail.ru>
2022-08-18 19:28:57 +0500

    fix after review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
+
 from reviews.models import Category, Comment, Genre, Review, Title
 
 from .filter import TitleFilter
@@ -17,22 +18,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
-        new_queryset = Review.objects.filter(title=title_id)
-        # TODO: ALEXEY
-        # Здесь стоит использовать get_object_or_404(), чтобы функция сразу
-        # выбросила 404, а мы не делали другие действия, а только потом
-        # выбрасывали.
-        # title=title_id -  конструкция рабочая, но не самая лучшая.
-        # Изначально Django ожидает, что мы ему дадим объект, а мы даем ID.
-        # Оно работает, потому что в базе хранятся именно ID объектов,
-        # а не сами объекты. Здесь лучше всегда явно через двойное
-        # подчеркивание указывать поле, которое нам необходимо.
-        # Пример:
-        # title__<имя_поля_из_модели>=<значение>
-        # (Поля pk, id как правило всегда присутствуют по умолчанию)
-        # В остальных местах в работе рекомендовал бы перейти на такой же
-        # способ, где это необходимо. Если мы сравниваем с ID, то значит стоит
-        # указать явно, что сравниваем с полем ID через двойной подчеркивание.
+        title = get_object_or_404(Title, pk=title_id)
+        new_queryset = Review.objects.filter(title=title)
         return new_queryset
 
     def perform_create(self, serializer):
@@ -46,12 +33,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (IsOwnerOrAdminOrReadOnly, )
 
     def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
         review_id = self.kwargs.get('review_id')
-        new_queryset = Comment.objects.filter(review=review_id)
-        # TODO: ALEXEY
-        # Здесь стоит использовать `get_object_or_404()
-        # Также стоит проверить, что это ревью на верный title_id.
-        # Иными словами надо добавить еще одно условие с проверкой на title_id.
+        review = get_object_or_404(Review, pk=review_id)
+        new_queryset = Comment.objects.filter(
+            review=review, review__title=title
+        )
         return new_queryset
 
     def perform_create(self, serializer):
@@ -62,13 +50,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all().annotate(rating=Avg('reviews__score'))
-    # TODO: ALEXEY
-    # Кстати, мы можем отсортировать данные по этому полю, которое создали
-    # в annotate. Можно посмотреть в сторону ordering_fields.
-    # https://www.django-rest-framework.org/api-guide/filtering/#specifying-which-fields-may-be-ordered-against
     permission_classes = (IsAdminOrReadOnly,)
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filterset_class = TitleFilter
+    ordering_fields = ('rating',)
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
